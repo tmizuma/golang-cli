4ce5ed212b9e4c8550914ddf285449926142f14e
tmizuma <rp008077@i.softbank.jp>
2023-12-09 03:10:37 +0900

    Refactor error handling in del function to avoid exit status 1

--- delcmd/file_operation.go
+++ delcmd/file_operation.go
@@ -15,7 +15,7 @@ func del(operator FileOperator, path string) error {
 	fileInfo, err := operator.Stat(path)
 	if err != nil {
 		fmt.Printf("failed to get file info: %v\n", err)
-		return err
+		return nil // return nil to avoid exit status 1
 	}
 	if fileInfo.IsDir() {
 		err = operator.RemoveAll(path)
